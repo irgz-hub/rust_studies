3e3956136234ddfdcd1c427ee69c9a9e0fe95045
Luigui Augusto <148299203+irgz-hub@users.noreply.github.com>
2023-12-06 02:08:52 -0300

    Delete 1_getting_started/hello_cargo/target directory

--- 1_getting_started/hello_cargo/target/debug/deps/hello_cargo-0bf9d8906d04252f.d
+++
@@ -1,5 +0,0 @@
-/home/irgz/Documents/development_and_programming/repositories/learning/rust_studies/1_getting_started/hello_cargo/target/debug/deps/libhello_cargo-0bf9d8906d04252f.rmeta: src/main.rs
-
-/home/irgz/Documents/development_and_programming/repositories/learning/rust_studies/1_getting_started/hello_cargo/target/debug/deps/hello_cargo-0bf9d8906d04252f.d: src/main.rs
-
-src/main.rs:
--- 1_getting_started/hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/irgz/Documents/development_and_programming/repositories/learning/rust_studies/1_getting_started/hello_cargo/target/debug/hello_cargo: /home/irgz/Documents/development_and_programming/repositories/learning/rust_studies/1_getting_started/hello_cargo/src/main.rs
