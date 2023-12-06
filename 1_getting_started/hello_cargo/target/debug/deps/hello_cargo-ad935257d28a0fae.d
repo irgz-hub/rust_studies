1caa30b51ccf95fdf6a920d66f40911cf9bb7fa1
irgz <luiguiaugusto.ls@gmail.com>
2023-12-06 01:57:05 -0300

    precap2

---
+++ 1_getting_started/hello_cargo/target/debug/deps/hello_cargo-ad935257d28a0fae.d
@@ -0,0 +1,5 @@
+/home/irgz/Documents/development_and_programming/repositories/learning/rust_studies/1_getting_started/hello_cargo/target/debug/deps/hello_cargo-ad935257d28a0fae: src/main.rs
+
+/home/irgz/Documents/development_and_programming/repositories/learning/rust_studies/1_getting_started/hello_cargo/target/debug/deps/hello_cargo-ad935257d28a0fae.d: src/main.rs
+
+src/main.rs:
