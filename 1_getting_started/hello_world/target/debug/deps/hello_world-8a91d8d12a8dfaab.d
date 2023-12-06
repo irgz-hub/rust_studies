28ab24f658f9074d8e928050f2fa2f1326add67d
Luigui Augusto <148299203+irgz-hub@users.noreply.github.com>
2023-12-06 02:09:14 -0300

    Delete 1_getting_started/hello_world/target directory

--- 1_getting_started/hello_world/target/debug/deps/hello_world-8a91d8d12a8dfaab.d
+++
@@ -1,5 +0,0 @@
-/home/irgz/Documents/development_and_programming/repositories/learning/rust_studies/1_getting_started/hello_world/target/debug/deps/hello_world-8a91d8d12a8dfaab: src/main.rs
-
-/home/irgz/Documents/development_and_programming/repositories/learning/rust_studies/1_getting_started/hello_world/target/debug/deps/hello_world-8a91d8d12a8dfaab.d: src/main.rs
-
-src/main.rs:
