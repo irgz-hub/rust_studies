b9b23541a0663dcdf2bab354e44417ac4a5b89f2
irgz <luiguiaugusto.ls@gmail.com>
2024-05-16 17:17:51 -0300

    chapter3 2.2

--- 3_common_programming_concepts/fibonacci_number/src/main.rs
+++ 3_common_programming_concepts/fibonacci_number/src/main.rs
@@ -5,7 +5,9 @@ fn main() {
 
     let mut nth: String = String::new();
 
-    io::stdin().read_line(&mut nth).expect("Failed to read line");
+    io::stdin()
+        .read_line(&mut nth)
+        .expect("Failed to read line");
 
     let nth: u64 = nth.trim().parse().expect("Type a number!");
 
@@ -15,7 +17,7 @@ fn main() {
         let mut n0: u64 = 0;
         let mut n1: u64 = 1;
         let mut count: u64 = 0;
-        while count < nth - 1{
+        while count < nth - 1 {
             fib_number = n0 + n1;
             n0 = n1;
             n1 = fib_number;
