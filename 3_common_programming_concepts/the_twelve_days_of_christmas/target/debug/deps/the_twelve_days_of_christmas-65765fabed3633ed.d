27b8bdc80d126ef3d1ca48db39a1a15c7740c2cc
irgz <luiguiaugusto.ls@gmail.com>
2024-05-16 17:16:32 -0300

    chapter3 2.1

--- 3_common_programming_concepts/the_twelve_days_of_christmas/target/debug/deps/the_twelve_days_of_christmas-65765fabed3633ed.d
+++
@@ -1,5 +0,0 @@
-/home/irgz/projects-wsl/personal/rust_studies/3_common_programming_concepts/the_twelve_days_of_christmas/target/debug/deps/libthe_twelve_days_of_christmas-65765fabed3633ed.rmeta: src/main.rs
-
-/home/irgz/projects-wsl/personal/rust_studies/3_common_programming_concepts/the_twelve_days_of_christmas/target/debug/deps/the_twelve_days_of_christmas-65765fabed3633ed.d: src/main.rs
-
-src/main.rs:
--- 3_common_programming_concepts/the_twelve_days_of_christmas/target/debug/deps/the_twelve_days_of_christmas-729eab40190d62cf.d
+++
@@ -1,5 +0,0 @@
-/home/irgz/projects-wsl/personal/rust_studies/3_common_programming_concepts/the_twelve_days_of_christmas/target/debug/deps/libthe_twelve_days_of_christmas-729eab40190d62cf.rmeta: src/main.rs
-
-/home/irgz/projects-wsl/personal/rust_studies/3_common_programming_concepts/the_twelve_days_of_christmas/target/debug/deps/the_twelve_days_of_christmas-729eab40190d62cf.d: src/main.rs
-
-src/main.rs:
